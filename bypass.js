d984cd81d7caeef1d797fa15f82113846db56b6c
Mr0xBEF <wanhnc@gmail.com>
2025-08-19 16:27:58 +0700

    Update bypass.js

--- bypass.js
+++ bypass.js
@@ -1,29 +1,60 @@
 
-if (typeof GM_setClipboard != 'function') alert('Your UserScript client has no GM_setClipboard support');
-
-(function() {
+(function () {
     'use strict';
 
-    let evaljs = document.body.innerHTML.match(/eval.*/)[0];
-    let unpackedjs = unPack(evaljs);
-    if (!unpackedjs.match(/"code":"(\w+)"/)){
-        let acefile_id = window.location.href.match(/^https?:\/\/acefile\.co\/(?:f|player)\/(\d+)/)[1]
-        $.ajax({
-            url: "https://acefile.co/service/resource_check/"+acefile_id+"/",
-            cache: false,
-            dataType: "json",
-            success: function(response){
-                let _id = response.data
-                let drivelink = 'https://drive.google.com/file/d/'+_id+'/view';
-                GM_setClipboard(drivelink);
-                window.location.href = drivelink;
-            }
-        });
+    if (typeof GM_setClipboard !== 'function') {
+        alert('Clipboard function (GM_setClipboard) is not supported by your userscript manager.');
+        return;
     }
-    else {
-        let _id = atob(unpackedjs.match(/"code":"(\w+)"/)[1]);
-        let drivelink = 'https://drive.google.com/file/d/'+_id+'/view';
-        GM_setClipboard(drivelink);
-        window.location.href = drivelink;
+
+    /**
+     * Try to extract and decode Google Drive ID from the obfuscated JS
+     */
+    function extractIdFromEval() {
+        try {
+            const body = document.body.innerHTML;
+            const evalMatch = body.match(/eval.*/);
+            if (!evalMatch) return null;
+
+            const unpacked = unPack(evalMatch[0]);
+            const codeMatch = unpacked.match(/"code":"(\w+)"/);
+            return codeMatch ? atob(codeMatch[1]) : null;
+        } catch (e) {
+            return null;
+        }
     }
+
+    /**
+     * Fetch ID from Acefile API as fallback
+     */
+    async function fetchIdFromApi() {
+        const urlMatch = window.location.href.match(/^https?:\/\/acefile\.co\/(?:f|player)\/(\d+)/);
+        if (!urlMatch) return null;
+
+        const fileId = urlMatch[1];
+        try {
+            const res = await fetch(`https://acefile.co/service/resource_check/${fileId}/`);
+            const json = await res.json();
+            return json?.data || null;
+        } catch {
+            return null;
+        }
+    }
+
+    /**
+     * Main logic
+     */
+    (async () => {
+        let driveId = extractIdFromEval();
+        if (!driveId) {
+            driveId = await fetchIdFromApi();
+        }
+
+        if (driveId) {
+            const driveLink = `https://drive.google.com/file/d/${driveId}/view`;
+            GM_setClipboard(driveLink);
+            window.location.replace(driveLink);
+        }
+    })();
+
 })();
